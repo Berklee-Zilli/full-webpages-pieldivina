606107bf33666ee2325e1e433d986e69e4010770
Robi Espinoza <robiespinoza@Robis-MacBook-Pro.local>
2024-11-19 19:55:15 -0500

    Updated CSS and JS code

---
+++ task1/using-js-to-make-changes.js
@@ -0,0 +1,4 @@
+document.title = "Task 1 by Robi";
+document.querySelector("header h1").textContent = "TASK 1";
+document.getElementById("intro").innerHTML = "Currently watching Sex and the City for the first time, exploring different kinds of ice cream flavors and writing scores like my life depends on it.";
+document.querySelector("h2").textContent = "List of my Favorite TV Shows";
